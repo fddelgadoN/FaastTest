fabe589a349623ba1c46dadfd4d8080f397d5438
fddelgado <fddelgado@corporativo.pt>
2022-11-18 10:13:07 +0000

    - Add default values -Change test

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -10,12 +10,12 @@ from life_expectancy.utils import (change_dtypes, drop_backslash_from_column_nam
                    split_comma_separated_column, unpivot_year_columns)
 # pylint: enable=import-error
 
-def save_data(data:pd.DataFrame, save_location:str) -> None:
+def save_data(data:pd.DataFrame, save_location:str = DATA_LOCATION+SAVED_FILE) -> None:
     """Save the data to file"""
     data.to_csv(save_location, index=False)
 
 
-def load_data(file_name: str) -> pd.DataFrame:
+def load_data(file_name: str = DATA_LOCATION+FILE_NAME) -> pd.DataFrame:
     """Load the data into a pd"""
     return pd.read_csv(file_name, sep='\t')
 
@@ -57,6 +57,6 @@ def clean_data(
     return data
 
 if __name__ == "__main__": # pragma: no cover
-    dataframe = load_data(DATA_LOCATION+FILE_NAME)
+    dataframe = load_data()
     dataframe = clean_data(argv = sys.argv[1:], data=dataframe)
-    save_data(dataframe, DATA_LOCATION+SAVED_FILE)
+    save_data(dataframe)
--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,13 +1,15 @@
 """Tests for the cleaning module"""
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import clean_data, load_data, save_data
 from . import OUTPUT_DIR
 
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data(['pt'])
+    data = load_data()
+    data = clean_data(['pt'], data)
+    save_data(data)
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
