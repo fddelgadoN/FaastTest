7f78d3d6156f19b07173260f8249cc3e444d9052
fddelgado <fddelgado@corporativo.pt>
2022-12-12 14:33:26 +0000

    -Divide into multiple modules

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -5,20 +5,10 @@ from typing import List
 import pandas as pd
 
 # pylint: disable=import-error
-from constant import DATA_LOCATION, FILE_NAME, SAVED_FILE
 from utils import (change_dtypes, drop_backslash_from_column_name,
                    split_comma_separated_column, unpivot_year_columns)
+from file_manipulation import save_data, load_data
 # pylint: enable=import-error
-
-def save_data(data:pd.DataFrame, save_location:str = DATA_LOCATION+SAVED_FILE) -> None:
-    """Save the data to file"""
-    data.to_csv(save_location, index=False)
-
-
-def load_data(file_name: str = DATA_LOCATION+FILE_NAME) -> pd.DataFrame:
-    """Load the data into a pd"""
-    return pd.read_csv(file_name, sep='\t')
-
 
 def clean_data(
         argv : List[str],
