d201843c2cc0599afee5f9293e6611f0ccd6ae1c
fddelgado <fddelgado@corporativo.pt>
2022-12-23 12:05:29 +0000

    -Add new function that encapsules other functions

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -6,21 +6,22 @@ import pandas as pd
 
 from life_expectancy.data_access import load_data, save_data
 
-def _split_comma_separated_column(data: pd.DataFrame, column:int) -> pd.DataFrame:
-    data[data.columns[column].split(",")] = [x.split(',') for x in data.iloc[:, column]]
-    return data
-
 def _unpivot_year_columns(data: pd.DataFrame) -> pd.DataFrame:
     years_cols = [x for x in data.columns if x.strip().isnumeric()]
     data = pd.melt(data, id_vars=data.columns[0], value_vars=years_cols, \
         value_name='value', var_name='year')
     return data
 
-def _drop_backslash_from_column_name(data:pd.DataFrame, column:str) -> pd.DataFrame:
-    return data.rename(columns={column: column.partition('\\')[0]})
-
 def _filter_dataframe(data:pd.DataFrame, country:str) -> pd.DataFrame:
     return data.loc[data.region.str.upper() == country.upper()]
+
+def _process_columns(data: pd.DataFrame, column: str, col_index: int) -> pd.DataFrame:
+    data = data.rename(columns={column: column.partition('\\')[0]})
+    data = _unpivot_year_columns(data)
+    data[data.columns[col_index].split(",")] = [x.split(',') for x in data.iloc[:, col_index]]
+    data = data.rename(columns={'geo': 'region'})
+
+    return data
 
 def clean_data(
         argv : List[str],
@@ -43,15 +44,8 @@ def clean_data(
 
     assert data is not None, "Error extracting data is null"
 
-    #Drop irrelevant info from first column
-    data = _drop_backslash_from_column_name(data, data.columns[0])
-
-    #Unpivot the year columns
-    data = _unpivot_year_columns(data)
-
-    #Split first column that is comma separated
-    data = _split_comma_separated_column(data, 0)
-    data = data.rename(columns={'geo': 'region'})
+    #process column values
+    data = _process_columns(data, data.columns[0], 0)
 
     #Reorder columns to fit to expected output...
     data=data[['unit','sex','age','region','year','value']]
