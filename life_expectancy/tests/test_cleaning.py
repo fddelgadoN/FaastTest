e87bea34dd68a5b12443bd39718dad1503428d12
fddelgado <fddelgado@corporativo.pt>
2022-12-23 09:57:24 +0000

    -Remove unused data in beginning of file

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,14 +1,7 @@
 """Tests for the cleaning module"""
-import os
-import sys
-
 import pandas as pd
 from life_expectancy.cleaning import clean_data, load_data, save_data
 from . import OUTPUT_DIR
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-source_dir = os.path.abspath(os.path.join(current_dir, ".."))
-sys.path.append(source_dir)
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
