c22325f8b5b2430ebd9f4abaf5b1bdd881c9b647
fddelgado <fddelgado@corporativo.pt>
2023-01-20 09:49:20 +0000

    -fixtures in conftest

--- life_expectancy/tests/conftest.py
+++ life_expectancy/tests/conftest.py
@@ -2,8 +2,14 @@
 import pandas as pd
 import pytest
 
+from life_expectancy import data_access
+from life_expectancy.cleaning import clean_data
+
 from . import FIXTURES_DIR, OUTPUT_DIR
 
+DATA_LOCATION = OUTPUT_DIR/"eu_life_expectancy_raw.tsv"
+PT_FILE_NAME = OUTPUT_DIR/"pt_life_expectancy_expected.csv"
+EU_FILE_NAME = OUTPUT_DIR/"eu_life_expectancy_expected.csv"
 
 @pytest.fixture(autouse=True)
 def run_before_and_after_tests() -> None:
@@ -21,3 +27,24 @@ def run_before_and_after_tests() -> None:
 def pt_life_expectancy_expected() -> pd.DataFrame:
     """Fixture to load the expected output of the cleaning script"""
     return pd.read_csv(FIXTURES_DIR / "pt_life_expectancy_expected.csv")
+
+
+@pytest.fixture(scope="session")
+def data_cleaned():
+    """Fixture that loads cleans and saves the data for the tests
+
+    Returns:
+        pd.DataFrame: Cleaned data
+    """
+    data = data_access.load_data(DATA_LOCATION)
+    dataframe = data.iloc[:100]
+    data_access.save_data(dataframe, EU_FILE_NAME)
+    data = clean_data(argv = ["pt"], data=dataframe).reset_index(drop=True)
+    data_access.save_data(data, PT_FILE_NAME)
+
+    return data
+
+
+@pytest.fixture(scope="session")
+def eu_csv_file():
+    return pd.read_csv(DATA_LOCATION, sep="\t")
