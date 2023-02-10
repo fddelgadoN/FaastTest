fdbbc0b5404a031051c6ce7ce8df1af52ecac6a4
fddelgado <fddelgado@corporativo.pt>
2023-02-10 10:39:29 +0000

    -add test to save data

--- life_expectancy/tests/test_data_access.py
+++ life_expectancy/tests/test_data_access.py
@@ -1,6 +1,9 @@
 """Test functions to test the data access functions"""
-import pandas as pd
+from unittest.mock import Mock
+
+import pytest
 from life_expectancy import data_access
+import pandas as pd
 
 EU_FILE_NAME = "life_expectancy/data/eu_life_expectancy_expected.csv"
 
@@ -13,3 +16,15 @@ def test_load_data(eu_csv_file : pd.DataFrame) -> None:
     """
     data = data_access.load_data()
     assert pd.DataFrame.equals(eu_csv_file, data)
+
+def test_save_data(monkeypatch: pytest.MonkeyPatch) -> None:
+    """Test the save data function
+
+    Args:
+        eu_csv_file (Fixture): Fixture that loads the tsv file
+    """
+    mock_print = Mock(return_value='MonkeyPatch save')
+    monkeypatch.setattr( "life_expectancy.data_access.save_data", lambda _ : print(mock_print()))
+    dummy_data = pd.DataFrame()
+    data_access.save_data(dummy_data)
+    mock_print.assert_called()
